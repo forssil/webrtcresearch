a1ec934ca0237b1190331d83de680c0b348ad81b
Keith Yan <quzhouyxm@126.com>
2016-06-18 21:19:58 +0800

    Fix invalid access

--- webrtc/modules/audio_processing/aecm/aecm_core_c.c
+++ webrtc/modules/audio_processing/aecm/aecm_core_c.c
@@ -1075,7 +1075,7 @@ void NLP(AecmCore* aecm) {
         aecm->aecmfloat.ErrorSpectrum[2*i]*=aecm->aecmfloat.gain[i]*gain;
         aecm->aecmfloat.ErrorSpectrum[2*i+1]*=aecm->aecmfloat.gain[i]*gain;
     }
-    for (i=32; i<PART_LEN-1; i++) {
+    for (i=32; i<PART_LEN-2; i++) {
         gain = gainarray[i]*0.5f+0.15f*gainarray[i+1]+0.15f*gainarray[i-1]+0.1f*gainarray[i+2]+0.1f*gainarray[i-2];
         aecm->aecmfloat.gain[i] += 0.7f*(gain-aecm->aecmfloat.gain[i]);
         aecm->aecmfloat.ErrorSpectrum[2*i]*=aecm->aecmfloat.gain[i]*ratio;
