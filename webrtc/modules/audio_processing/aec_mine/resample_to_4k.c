6bbf73186c771f41234a6bbcb9042e1104b628e4
Keith Yan <qzyxm@126.com>
2016-09-07 03:35:54 -0400

    remove old code

--- webrtc/modules/audio_processing/aec_mine/resample_to_4k.c
+++
@@ -1,9 +0,0 @@
-//
-//  resample_to_4k.c
-//  webrtaecmut
-//
-//  Created by huagao on 4/21/16.
-//  Copyright © 2016 keith. All rights reserved.
-//
-
-#include "resample_to_4k.h"
--- webrtc/modules/audio_processing/aec_mine/time_delay_estimator.c
+++
@@ -1,9 +0,0 @@
-//
-//  time_delay_estimator.c
-//  webrtaecmut
-//
-//  Created by huagao on 4/25/16.
-//  Copyright © 2016 keith. All rights reserved.
-//
-
-#include "time_delay_estimator.h"
