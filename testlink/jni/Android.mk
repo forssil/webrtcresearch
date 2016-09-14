e3417da7bb39c33f15d7e3298c4e6af0f2e23473
Keith Yan <qzyxm@126.com>
2016-09-14 03:46:54 -0400

    Refine link

--- testlink/jni/Android.mk
+++ testlink/jni/Android.mk
@@ -1,136 +1,136 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_MODULE    := metrics_default
+LOCAL_MODULE    := libmetrics_default
 LOCAL_SRC_FILES := ../package/Debug/libmetrics_default.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := audio_coding_module
+LOCAL_MODULE    := libaudio_coding_module
 LOCAL_SRC_FILES := ../package/Debug/libaudio_coding_module.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := neteq
+LOCAL_MODULE    := libneteq
 LOCAL_SRC_FILES := ../package/Debug/libneteq.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := audio_conference_mixer
+LOCAL_MODULE    := libaudio_conference_mixer
 LOCAL_SRC_FILES := ../package/Debug/libaudio_conference_mixer.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := openmax_dl
+LOCAL_MODULE    := libopenmax_dl
 LOCAL_SRC_FILES := ../package/Debug/libopenmax_dl.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := audio_decoder_interface
+LOCAL_MODULE    := libaudio_decoder_interface
 LOCAL_SRC_FILES := ../package/Debug/libaudio_decoder_interface.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := openmax_dl_armv7
+LOCAL_MODULE    := libopenmax_dl_armv7
 LOCAL_SRC_FILES := ../package/Debug/libopenmax_dl_armv7.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := audio_device
+LOCAL_MODULE    := libaudio_device
 LOCAL_SRC_FILES := ../package/Debug/libaudio_device.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := openmax_dl_neon
+LOCAL_MODULE    := libopenmax_dl_neon
 LOCAL_SRC_FILES := ../package/Debug/libopenmax_dl_neon.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := audio_encoder_interface
+LOCAL_MODULE    := libaudio_encoder_interface
 LOCAL_SRC_FILES := ../package/Debug/libaudio_encoder_interface.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := opus
+LOCAL_MODULE    := libopus
 LOCAL_SRC_FILES := ../package/Debug/libopus.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := audioproc_debug_proto
+LOCAL_MODULE    := libaudioproc_debug_proto
 LOCAL_SRC_FILES := ../package/Debug/libaudioproc_debug_proto.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := paced_sender
+LOCAL_MODULE    := libpaced_sender
 LOCAL_SRC_FILES := ../package/Debug/libpaced_sender.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := audio_processing
+LOCAL_MODULE    := libaudio_processing
 LOCAL_SRC_FILES := ../package/Debug/libaudio_processing.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := pcm16b
+LOCAL_MODULE    := libpcm16b
 LOCAL_SRC_FILES := ../package/Debug/libpcm16b.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := audio_processing_neon
+LOCAL_MODULE    := libaudio_processing_neon
 LOCAL_SRC_FILES := ../package/Debug/libaudio_processing_neon.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := protobuf_lite
+LOCAL_MODULE    := libprotobuf_lite
 LOCAL_SRC_FILES := ../package/Debug/libprotobuf_lite.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := bitrate_controller
+LOCAL_MODULE    := libbitrate_controller
 LOCAL_SRC_FILES := ../package/Debug/libbitrate_controller.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := red
+LOCAL_MODULE    := libred
 LOCAL_SRC_FILES := ../package/Debug/libred.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := cng
+LOCAL_MODULE    := libcng
 LOCAL_SRC_FILES := ../package/Debug/libcng.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := remote_bitrate_estimator
+LOCAL_MODULE    := libremote_bitrate_estimator
 LOCAL_SRC_FILES := ../package/Debug/libremote_bitrate_estimator.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := common_audio
+LOCAL_MODULE    := libcommon_audio
 LOCAL_SRC_FILES := ../package/Debug/libcommon_audio.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := rent_a_codec
+LOCAL_MODULE    := librent_a_codec
 LOCAL_SRC_FILES := ../package/Debug/librent_a_codec.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := common_audio_neon
+LOCAL_MODULE    := libcommon_audio_neon
 LOCAL_SRC_FILES := ../package/Debug/libcommon_audio_neon.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := rtc_base
+LOCAL_MODULE    := librtc_base
 LOCAL_SRC_FILES := ../package/Debug/librtc_base.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := cpu_features
+LOCAL_MODULE    := libcpu_features
 LOCAL_SRC_FILES := ../package/Debug/libcpu_features.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := rtc_base_approved
+LOCAL_MODULE    := librtc_base_approved
 LOCAL_SRC_FILES := ../package/Debug/librtc_base_approved.a
 include $(PREBUILT_STATIC_LIBRARY)
 
@@ -140,99 +140,99 @@ LOCAL_SRC_FILES := ../package/Debug/libcpu_features_android.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := rtc_event_log
+LOCAL_MODULE    := librtc_event_log
 LOCAL_SRC_FILES := ../package/Debug/librtc_event_log.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := g711
+LOCAL_MODULE    := libg711
 LOCAL_SRC_FILES := ../package/Debug/libg711.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := rtc_event_log_proto
+LOCAL_MODULE    := librtc_event_log_proto
 LOCAL_SRC_FILES := ../package/Debug/librtc_event_log_proto.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := g722
+LOCAL_MODULE    := libg722
 LOCAL_SRC_FILES := ../package/Debug/libg722.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := rtp_rtcp
+LOCAL_MODULE    := librtp_rtcp
 LOCAL_SRC_FILES := ../package/Debug/librtp_rtcp.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := gflags
+LOCAL_MODULE    := libgflags
 LOCAL_SRC_FILES := ../package/Debug/libgflags.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := ilbc
+LOCAL_MODULE    := libilbc
 LOCAL_SRC_FILES := ../package/Debug/libilbc.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := system_wrappers
+LOCAL_MODULE    := libsystem_wrappers
 LOCAL_SRC_FILES := ../package/Debug/libsystem_wrappers.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := isac
+LOCAL_MODULE    := libisac
 LOCAL_SRC_FILES := ../package/Debug/libisac.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := voice_engine
+LOCAL_MODULE    := libvoice_engine
 LOCAL_SRC_FILES := ../package/Debug/libvoice_engine.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := isac_neon
+LOCAL_MODULE    := libisac_neon
 LOCAL_SRC_FILES := ../package/Debug/libisac_neon.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := webrtc_common
+LOCAL_MODULE    := libwebrtc_common
 LOCAL_SRC_FILES := ../package/Debug/libwebrtc_common.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := level_indicator
+LOCAL_MODULE    := liblevel_indicator
 LOCAL_SRC_FILES := ../package/Debug/liblevel_indicator.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := webrtc_opus
+LOCAL_MODULE    := libwebrtc_opus
 LOCAL_SRC_FILES := ../package/Debug/libwebrtc_opus.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := media_file
+LOCAL_MODULE    := libmedia_file
 LOCAL_SRC_FILES := ../package/Debug/libmedia_file.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := webrtc_utility
+LOCAL_MODULE    := libwebrtc_utility
 LOCAL_SRC_FILES := ../package/Debug/libwebrtc_utility.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := rtc_task_queue
+LOCAL_MODULE    := librtc_task_queue
 LOCAL_SRC_FILES := ../package/Debug/librtc_task_queue.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := audio_decoder_factory_interface
+LOCAL_MODULE    := libaudio_decoder_factory_interface
 LOCAL_SRC_FILES := ../package/Debug/libaudio_decoder_factory_interface.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := static_add
+LOCAL_MODULE    := libstatic_add
 LOCAL_SRC_FILES := ../package/Debug/libstatic_add.a
 include $(PREBUILT_STATIC_LIBRARY)
 
@@ -254,11 +254,11 @@ LOCAL_CFLAGS += -DWEBRTC_INCLUDE_INTERNAL_AUDIO_DEVICE -DOPUS_FIXED_POINT
 LOCAL_CFLAGS += -DUSE_LIBPCI=1 -DUSE_OPENSSL=1 -DUSE_OPENSSL_CERTS=1
 LOCAL_CFLAGS += -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -DANDROID
 LOCAL_CFLAGS += -D__GNU_SOURCE=1 '-DCHROME_BUILD_ID=""' -DHAVE_SYS_UIO_H
-LOCAL_CFLAGS += -DDYNAMIC_ANNOTATIONS_ENABLED=1 -DWTF_USE_DYNAMIC_ANNOTATIONS=1 -D_DEBUG
+LOCAL_CFLAGS += -DDYNAMIC_ANNOTATIONS_ENABLED=1 -DWTF_USE_DYNAMIC_ANNOTATIONS=1
 
 LOCAL_C_INCLUDES := ../package/include/
 LOCAL_MODULE    := webrtc_voe_wrapper
-LOCAL_STATIC_LIBRARIES += static_add libstlport
+LOCAL_STATIC_LIBRARIES += libstatic_add libmetrics_default libaudio_coding_module libneteq libaudio_conference_mixer libopenmax_dl libaudio_decoder_interface libopenmax_dl_armv7 libaudio_device libopenmax_dl_neon libaudio_encoder_interface libopus libaudioproc_debug_proto libpaced_sender libaudio_processing libpcm16b libaudio_processing_neon libprotobuf_lite libbitrate_controller libred libcng libremote_bitrate_estimator libcommon_audio librent_a_codec libcommon_audio_neon librtc_base libcpu_features librtc_base_approved libcpu_features_android librtc_event_log libg711 librtc_event_log_proto libg722 librtp_rtcp libgflags libilbc libsystem_wrappers libisac libvoice_engine ibisac_neon libwebrtc_common  liblevel_indicator libwebrtc_opus libmedia_file libwebrtc_utility librtc_task_queue libaudio_decoder_factory_interface 
 
 
 LOCAL_SRC_FILES := voewrapper.cpp
--- testlink/jni/Application.mk
+++ testlink/jni/Application.mk
@@ -1,5 +1,5 @@
 APP_MODULES:=webrtc_voe_wrapper
-APP_STL :=stlport_static
-APP_ABI:= armeabi-v7a
-APP_CPPFLAGS += -fexceptions
-
+APP_ABI := armeabi-v7a
+APP_STL := c++_shared
+APP_PLATFORM := android-16
+APP_CPPFLAGS += -frtti
